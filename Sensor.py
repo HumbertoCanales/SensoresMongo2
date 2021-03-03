abffca3ed68ca9ae76748c16a358b435b6e07b9b
Alex Lozano <19170025@uttcampus.edu.mx>
2021-03-03 00:19:08 -0600

    Registros con fecha mongodb

--- Sensor.py
+++ Sensor.py
@@ -1,16 +1,18 @@
 class Sensor():
-    def __init__(self,nombre,valor,tipo):
+    def __init__(self,nombre,valor,tipo,Fecha):
         self.nombre=nombre
         self.valor=valor
         self.tipo=tipo
+        self.fecha=Fecha
 
     def getDocument(self):
         document = {
             "nombre":self.nombre,
             "valor":self.valor,
-            "tipo":self.tipo
+            "tipo":self.tipo,
+            "fecha":self.fecha
         }
         return document
 
     def getTupla(self):
-        return (self.nombre,self.valor,self.tipo)
+        return (self.nombre,self.valor,self.tipo,self.fecha)
