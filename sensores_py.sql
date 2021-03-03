2d12f8d0db57f02dd384c77ea390450ea0147beb
HumbertoCanales <19170146@uttcampus.edu.mx>
2021-03-02 21:42:07 -0600

    Database file

---
+++ sensores_py.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `sensores` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `tipo_dato` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4
+
+CREATE TABLE `valores` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `sensor_id` int(10) unsigned NOT NULL,
+  `flotante` float DEFAULT NULL,
+  `booleano` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `valores_sensor_id` (`sensor_id`),
+  CONSTRAINT `valores_sensor_id` FOREIGN KEY (`sensor_id`) REFERENCES `sensores` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4
